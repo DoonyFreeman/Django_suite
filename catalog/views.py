084b51ce26777482d568de5dfb40a69ab7fb0ae9
DoonyFreeman <rebrikov2006@gmail.com>
2026-02-12 16:09:11 +0300

    added templates

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def product_list(request):              # Функция для обработки запроса на главную страницу каталога (catalog)
-
-    return HttpResponse("Страница со списком товаров")
+    template_name = 'catalog/product_list.html'
+    return render(request, template_name)
 
 def product_category(request, category):            # Функция для обработки запроса на страницу категории товаров (catalog/something)
     return HttpResponse(f"Категория {category}")
