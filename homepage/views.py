2f467eb4e28be173fbb4c1e80a2f784ca4b71962
DoonyFreeman <rebrikov2006@gmail.com>
2026-02-12 22:29:42 +0300

    added vievs feature

--- homepage/views.py
+++ homepage/views.py
@@ -6,7 +6,13 @@ from django.template import loader
 def index(request):
     # Подключаем HTML-файл.
     template_name = 'homepage/index.html'
+    title = 'Главная страница ACME'
+    promo_product = 'Iron Carrot'
+    context = {
+        'title': title,
+        'promo_product': promo_product,
+    }
     # Передаём в объект HttpResponse 
     # HTML-код из загруженного файла, объект запроса request;
     # и возвращаем этот объект.
-    return render(request, template_name)
+    return render(request, template_name, context)
