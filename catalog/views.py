acf302dd13ebcb84514f862d687de1571a56d2f3
DoonyFreeman <rebrikov2006@gmail.com>
2026-02-12 22:54:47 +0300

    added cycles, if in views catalog

--- catalog/views.py
+++ catalog/views.py
@@ -3,7 +3,18 @@ from django.http import HttpResponse
 # Create your views here.
 def product_list(request):              # Функция для обработки запроса на главную страницу каталога (catalog)
     template_name = 'catalog/product_list.html'
-    return render(request, template_name)
+    title = 'Список товаров ACME'
+    products = [
+        'Iron carrot',
+        'Giant mousetrap',
+        'Dehydrated boulders',
+        'Invisible paint',
+    ]
+    context = {
+        'title': title,
+        'products': products,
+    }
+    return render(request, template_name, context)
 
 def product_category(request, category):            # Функция для обработки запроса на страницу категории товаров (catalog/something)
     return HttpResponse(f"Категория {category}")
